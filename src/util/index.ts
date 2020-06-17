1725037c6c134423a3296c70741e3a38509493b5
zhounie <iszhounie@aliyun.com>
2020-06-17 10:42:39 +0800

    修改eslint报错

--- src/util/index.ts
+++ src/util/index.ts
@@ -26,40 +26,40 @@ export const setTree = (arr: any[], parentId: string | number) => {
 export const full = () => {
     if (document.fullscreenElement) {
         if (document.exitFullscreen) {
-            document.exitFullscreen();
+            document.exitFullscreen()
         }
     } else {
         if (document.documentElement.requestFullscreen) {
-            document.documentElement.requestFullscreen();
+            document.documentElement.requestFullscreen()
         }
     }
 }
 
 
-export function noop() {};
+export function noop() {}
 
-export function getPropByPath(obj, path, strict) {
+export function getPropByPath(obj: any, path: any, strict: any): any {
 	let tempObj = obj;
-	path = path.replace(/\[(\w+)\]/g, '.$1');
-	path = path.replace(/^\./, '');
+	path = path.replace(/\[(\w+)\]/g, '.$1')
+	path = path.replace(/^\./, '')
   
-	let keyArr = path.split('.');
+	let keyArr = path.split('.')
 	let i = 0;
 	for (let len = keyArr.length; i < len - 1; ++i) {
-	  if (!tempObj && !strict) break;
-	  let key = keyArr[i];
-	  if (key in tempObj) {
-		tempObj = tempObj[key];
-	  } else {
+        if (!tempObj && !strict) break
+        let key = keyArr[i]
+        if (key in tempObj) {
+            tempObj = tempObj[key]
+        } else {
 		if (strict) {
-		  throw new Error('please transfer a valid prop path to form item!');
+            throw new Error('please transfer a valid prop path to form item!');
 		}
-		break;
-	  }
+        break;
+        }
 	}
 	return {
-	  o: tempObj,
-	  k: keyArr[i],
-	  v: tempObj ? tempObj[keyArr[i]] : null
-	};
-  };
+        o: tempObj,
+        k: keyArr[i],
+        v: tempObj ? tempObj[keyArr[i]] : null
+	}
+}
